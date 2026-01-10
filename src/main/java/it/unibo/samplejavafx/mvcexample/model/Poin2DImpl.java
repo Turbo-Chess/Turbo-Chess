d667486a6d0cf5379cae80ac6caf31a4117071cb
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-10 22:10:34 +0100

    Added Point2D classes

---
+++ src/main/java/it/unibo/samplejavafx/mvcexample/model/Poin2DImpl.java
@@ -0,0 +1,4 @@
+package it.unibo.samplejavafx.mvcexample.model;
+
+public class Poin2DImpl implements Point2D {
+}
---
+++ src/main/java/it/unibo/samplejavafx/mvcexample/model/Point2D.java
@@ -0,0 +1,4 @@
+package it.unibo.samplejavafx.mvcexample.model;
+
+public interface Point2D {
+}
