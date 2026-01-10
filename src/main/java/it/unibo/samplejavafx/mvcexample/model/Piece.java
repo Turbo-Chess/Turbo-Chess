bebcf114ad4ca5444db4c376eaaae34635e82476
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-10 23:28:57 +0100

    Added piece class. Not already complete

---
+++ src/main/java/it/unibo/samplejavafx/mvcexample/model/Piece.java
@@ -0,0 +1,35 @@
+package it.unibo.samplejavafx.mvcexample.model;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Piece extends AbstractEntity{
+    private final int weight;
+    private boolean hasMoved = false;
+    private final List<Point2D> avaliableCells = new ArrayList<>();
+    private final List<MoveRules> moveRules;
+
+    Piece(final String id, final String name, final Path path, final boolean whiteColor, final int weight, final List<MoveRules> moveRules) {
+        super(id, name, path, whiteColor);
+        this.moveRules = moveRules;
+        this.weight = weight;
+    }
+
+    public List<Point2D> getValidMoves() {
+        // TODO: implement method
+        return this.avaliableCells;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public void setHasMoved() {
+        this.hasMoved = true;
+    }
+}
