dd128e93b79f5f36a214b7d177990f08a81c7ed8
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-12 23:27:08 +0100

    Implemented move filter rule directly into the enum, so it can be used as filter

---
+++ src/main/java/it/unibo/samplejavafx/mvc/model/movement/MoveRulesImpl.java
@@ -0,0 +1,55 @@
+package it.unibo.samplejavafx.mvc.model.movement;
+
+import it.unibo.samplejavafx.mvc.model.chessboard.ChessBoard;
+import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
+
+
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class MoveRulesImpl implements MoveRules {
+    private final Point2D direction;
+    private final MoveType restriction;
+    private final MovementStrategy moveStrategy;
+
+    public MoveRulesImpl(final Point2D direction, final MoveType restriction, final MovementStrategy moveStrategy) {
+        this.direction = direction;
+        this.restriction = restriction;
+        this.moveStrategy = moveStrategy;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Point2D> getValidMoves(final Point2D start, final ChessBoard board, final boolean isWhite) {
+        List<Point2D> tempResult = moveStrategy.calculateMoves(start, this.direction, board);
+        return List.of();
+    }
+
+
+    public Point2D getDirection() {
+        return direction;
+    }
+
+    public MoveType getRestriction() {
+        return restriction;
+    }
+
+    public MovementStrategy getMoveStrategy() {
+        return moveStrategy;
+    }
+
+    public enum MoveType {
+        MOVE_ONLY(ChessBoard::isFree),
+        EAT_ONLY((board, pos) -> !board.isFree(pos)),
+        MOVE_AND_EAT((board, pos) -> true);
+
+        private final BiPredicate<ChessBoard, Point2D> filterFunc;
+
+        MoveType(final BiPredicate<ChessBoard, Point2D> filterFunc) {
+            this.filterFunc = filterFunc;
+        }
+    }
+}
