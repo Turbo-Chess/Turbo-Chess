5a043dcb38a6db9bb753d964e58c76a15a4587ee
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-12 23:26:37 +0100

    Added the "isFree" method to the chessboard interface, so it became a functional interface

--- src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
@@ -1,8 +1,11 @@
 package it.unibo.samplejavafx.mvc.model.chessboard;
 
+import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
+
 /**
  * Represents the board where the match is played.
  */
+@FunctionalInterface
 public interface ChessBoard {
-
+    boolean isFree(final Point2D pos);
 }
