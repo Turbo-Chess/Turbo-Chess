6a18544d9dd93d44acacacd5077a4963d3eeac56
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-14 21:43:08 +0100

    Moved the size constant into the implementation of the chessboard implementation

--- src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
@@ -5,9 +5,6 @@ import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 /**
  * Represents the board where the match is played.
  */
-@FunctionalInterface
 public interface ChessBoard {
-    int CHESSBOARD_SIZE = 8;
-
     boolean isFree(final Point2D pos);
 }
