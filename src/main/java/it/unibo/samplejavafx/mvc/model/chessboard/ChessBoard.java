a5a12add988025d4d5e8425ca3b51a51f6cbd42d
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-15 22:30:51 +0100

    Added the setEntity() method to chessboard interface

--- src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/chessboard/ChessBoard.java
@@ -1,5 +1,6 @@
 package it.unibo.samplejavafx.mvc.model.chessboard;
 
+import it.unibo.samplejavafx.mvc.model.entity.Entity;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 
 /**
@@ -7,6 +8,8 @@ import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
  */
 public interface ChessBoard {
     boolean isFree(final Point2D pos);
+
+    void setEntity(final Point2D pos, final Entity newEntity);
 
     boolean checkBounds(Point2D pos);
 }
