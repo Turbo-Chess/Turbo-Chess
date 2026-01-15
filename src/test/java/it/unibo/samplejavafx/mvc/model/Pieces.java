fc349175a9b2a216afabebcbd3e3370fac74bb48
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-15 23:12:01 +0100

    Made tests for the piece class

---
+++ src/test/java/it/unibo/samplejavafx/mvc/model/Pieces.java
@@ -0,0 +1,29 @@
+package it.unibo.samplejavafx.mvc.model;
+
+import it.unibo.samplejavafx.mvc.model.entity.Piece;
+import it.unibo.samplejavafx.mvc.model.movement.JumpingMovement;
+import it.unibo.samplejavafx.mvc.model.movement.MoveRulesImpl;
+import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class Pieces {
+    @Test
+    public void testPieceInitialization() {
+        final Piece p = new Piece("test", "test-piece", Path.of("/home/giacomo/Documents/pawn.jpg"), false,
+                3, List.of(
+                        new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, new JumpingMovement())
+        ));
+
+        assertEquals("test", p.getId());
+        assertEquals("test-piece", p.getName());
+        assertEquals(Path.of("/home/giacomo/Documents/pawn.jpg"), p.getImagePath());
+        assertFalse(p.isHasMoved());
+        assertEquals(3, p.getWeight());
+    }
+}
