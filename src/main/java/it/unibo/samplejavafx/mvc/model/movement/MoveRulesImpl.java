c5b615ccb693032b3b2907123a8e5e3fc0372c29
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-17 16:36:48 +0100

    Modified MoveRulesImpl to check conditions in the correct way (in the previous implementation some check was not performed)

--- src/main/java/it/unibo/samplejavafx/mvc/model/movement/MoveRulesImpl.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/movement/MoveRulesImpl.java
@@ -1,11 +1,14 @@
 package it.unibo.samplejavafx.mvc.model.movement;
 
 import it.unibo.samplejavafx.mvc.model.chessboard.ChessBoard;
+import it.unibo.samplejavafx.mvc.model.entity.Entity;
+import it.unibo.samplejavafx.mvc.model.entity.Piece;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 import lombok.Getter;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
@@ -27,21 +30,54 @@ public class MoveRulesImpl implements MoveRules {
     @Override
     public List<Point2D> getValidMoves(final Point2D start, final ChessBoard board, final boolean isWhite) {
         List<Point2D> tempResult = moveStrategy.calculateMoves(start, isWhite ? direction.invertY() : direction, board);
+        return switch (restriction) {
+            case MOVE_ONLY -> moveOnlyFilter(board, tempResult);
+            case EAT_ONLY -> eatOnlyFilter(board, tempResult, isWhite);
+            case MOVE_AND_EAT -> moveAndEatFilter(board, tempResult, isWhite);
+        };
+
+        // This can't be reached because all rules MUST be one of the 3 in the MoveType enum
+    }
+
+    private List<Point2D> moveOnlyFilter(ChessBoard board, final List<Point2D> tempResult) {
         return tempResult.stream()
-                .filter(pos -> this.restriction.filterFunc.test(board, pos))
+                .filter(board::isFree)
                 .toList();
+    }
+
+    private List<Point2D> eatOnlyFilter(ChessBoard board, final List<Point2D> tempResult, boolean isWhite) {
+        return tempResult.stream()
+                .filter(pos -> !board.isFree(pos))
+                .filter(pos -> !board.getEntity(pos).get().isWhite() == isWhite)
+                .toList();
+    }
+
+    private List<Point2D> moveAndEatFilter(ChessBoard board, final List<Point2D> tempResult, boolean isWhite) {
+        for (var pos : tempResult) {
+            final Optional<Entity> pieceInNewPos = board.getEntity(pos);
+            if (pieceInNewPos.isPresent() && pieceInNewPos.get().isWhite() == isWhite) {
+                tempResult.remove(pos);
+            }
+        }
+
+        return tempResult;
     }
 
 
     public enum MoveType {
-        MOVE_ONLY(ChessBoard::isFree),
-        EAT_ONLY((board, pos) -> !board.isFree(pos)),
-        MOVE_AND_EAT((board, pos) -> true);
-
-        private final BiPredicate<ChessBoard, Point2D> filterFunc;
-
-        MoveType(final BiPredicate<ChessBoard, Point2D> filterFunc) {
-            this.filterFunc = filterFunc;
-        }
+        MOVE_ONLY,
+        EAT_ONLY,
+        MOVE_AND_EAT;
     }
 }
+
+/*MOVE_ONLY(ChessBoard::isFree),
+ EAT_ONLY((board, pos) -> !board.isFree(pos) && (board.getEntity().)),
+ MOVE_AND_EAT((board, pos) -> true);
+
+ private final BiPredicate<ChessBoard, Point2D> filterFunc;
+
+ MoveType(final BiPredicate<ChessBoard, Point2D> filterFunc) {
+ this.filterFunc = filterFunc;
+ }
+}*/
