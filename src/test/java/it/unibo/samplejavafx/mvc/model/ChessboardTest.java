0ed1a287a4fd29704d13564e66f45543eea1e6c4
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-01-26 23:12:27 +0100

    Updated tests to verify the correctness of the new methods.

--- src/test/java/it/unibo/samplejavafx/mvc/model/ChessboardTest.java
+++ src/test/java/it/unibo/samplejavafx/mvc/model/ChessboardTest.java
@@ -23,7 +23,7 @@ class ChessboardTest {
      */
     @Test
    void initTest() {
-        final Piece p = new Piece("test", "test-piece", Path.of("/home/giacomo/Documents/pawn.jpg"), PlayerColor.BLACK,
+        final Piece p = new Piece("test", "test-piece", 1,  Path.of("/home/giacomo/Documents/pawn.jpg"), PlayerColor.BLACK,
                 3, List.of(
                 new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, new JumpingMovement())
         ));
@@ -33,6 +33,9 @@ class ChessboardTest {
         assertTrue(board.isFree(new Point2D(2, 2)));
         board.setEntity(new Point2D(2, 2), p);
         assertFalse(board.isFree(new Point2D(2, 2)));
+
+        board.removeEntity(new Point2D(2, 2));
+        assertTrue(board.isFree(new Point2D(2, 2)));
 
         assertTrue(board.isFree(new Point2D(5, 3)));
         board.setEntity(new Point2D(5, 3), p);
@@ -50,5 +53,25 @@ class ChessboardTest {
         assertTrue(board.isFree(new Point2D(0, 0)));
         assertTrue(board.isFree(new Point2D(7, 7)));
         assertThrows(NullPointerException.class, () -> board.isFree(new Point2D(8, 8)));
+    }
+
+    @Test
+    void checkGetPos() {
+        final ChessBoard board = new ChessBoardImpl();
+        final Piece p1 = new Piece("test", "test-piece", 1, Path.of("/home/giacomo/Documents/pawn.jpg"), PlayerColor.BLACK,
+                3, List.of(
+                new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, new JumpingMovement())
+        ));
+
+        final Piece p2 = new Piece("test", "test-piece", 2, Path.of("/home/giacomo/Documents/pawn.jpg"), PlayerColor.BLACK,
+                3, List.of(
+                new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, new JumpingMovement())
+        ));
+
+       board.setEntity(new Point2D(2, 2), p1);
+       board.setEntity(new Point2D(3, 3), p2);
+
+       assertEquals(new Point2D(2, 2), board.getPosByEntity(p1));
+       assertEquals(new Point2D(3, 3), board.getPosByEntity(p2));
     }
 }
