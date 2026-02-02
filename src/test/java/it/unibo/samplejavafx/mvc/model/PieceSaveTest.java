330e66c2171171876b5c5be0c94121acc3ba0fc6
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-02 16:26:43 +0100

    Made a test to verify Jackson work

--- src/test/java/it/unibo/samplejavafx/mvc/model/PieceSaveTest.java
+++ src/test/java/it/unibo/samplejavafx/mvc/model/PieceSaveTest.java
@@ -1,31 +1,41 @@
 package it.unibo.samplejavafx.mvc.model;
 
-import com.google.gson.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import it.unibo.samplejavafx.mvc.model.entity.Piece;
 import it.unibo.samplejavafx.mvc.model.entity.PlayerColor;
-import it.unibo.samplejavafx.mvc.model.movement.JumpingMovement;
 import it.unibo.samplejavafx.mvc.model.movement.MoveRulesImpl;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PieceSaveTest {
     @Test
     void test() throws IOException {
-        var jp = new JumpingMovement();
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         Piece p = new Piece("test", "test-piece", 1, "/home/giacomo/Documents/pawn.jpg", PlayerColor.BLACK,
                 3, List.of(
                 new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, MoveRulesImpl.MoveStrategy.JUMPING)
         ));
 
-        try (FileWriter writer = new FileWriter("/home/giacomo/Documents/Universita/SecondoAnno/PrimoSemestre/ProgrammazioneOggetti/Turbo-Chess/src/main/resources/EntityResources/StandardChessPieces/Pawn.json")) {
-            gson.toJson(p, writer);
+            mapper.writeValue(new File("src/main/resources/EntityResources/StandardChessPieces/pieces/Pawn.json"), p);
         }
+
+    @Test
+    void testInit() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Piece pInit = mapper.readValue(new File("src/main/resources/EntityResources/StandardChessPieces/pieces/Pawn.json"), Piece.class);
+        Piece p = new Piece("test", "test-piece", 1, "/home/giacomo/Documents/pawn.jpg", PlayerColor.BLACK,
+                3, List.of(
+                new MoveRulesImpl(new Point2D(0, 1), MoveRulesImpl.MoveType.MOVE_AND_EAT, MoveRulesImpl.MoveStrategy.JUMPING)
+        ));
+        assertEquals(p, pInit);
     }
 }
