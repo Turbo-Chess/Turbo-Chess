84b408ca37c98670fbb923548ecc359b3b621a03
Luca Bevilacqua <luca.bevilacqua6@studio.unibo.it>
2026-02-04 22:30:24 +0100

    add Entity to MoveEvent

--- src/main/java/it/unibo/samplejavafx/mvc/model/replay/MoveEvent.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/replay/MoveEvent.java
@@ -1,5 +1,6 @@
 package it.unibo.samplejavafx.mvc.model.replay;
 
+import it.unibo.samplejavafx.mvc.model.entity.Entity;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 
 /**
@@ -9,7 +10,7 @@ import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
  * @param from source position.
  * @param to destination position.
  */
-public record MoveEvent(int turn, Point2D from, Point2D to) implements GameEvent {
+public record MoveEvent(int turn, Entity entity, Point2D from, Point2D to) implements GameEvent {
     @Override
     public int getTurn() {
         return turn;
