e8c46ac1efd4d59961309a896982a046feefde53
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-04 15:50:40 +0100

    Created two JSON views, one for initial loading (used to not load "runtime" fields) and one for loading saved game states

---
+++ src/main/java/it/unibo/samplejavafx/mvc/model/Loader/JsonViews.java
@@ -0,0 +1,6 @@
+package it.unibo.samplejavafx.mvc.model.Loader;
+
+public class JsonViews {
+    public static class FirstLoading {};
+    public static class FullLoading extends FirstLoading {};
+}
