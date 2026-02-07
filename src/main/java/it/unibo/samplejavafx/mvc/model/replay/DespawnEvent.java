79a2ac6f71a70dcffc959ac5b26ff69f990df4cc
Luca Bevilacqua <luca.bevilacqua6@studio.unibo.it>
2026-02-07 13:03:02 +0100

    refactor(model): add entity field to DespawnEvent record

--- src/main/java/it/unibo/samplejavafx/mvc/model/replay/DespawnEvent.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/replay/DespawnEvent.java
@@ -1,14 +1,16 @@
 package it.unibo.samplejavafx.mvc.model.replay;
 
+import it.unibo.samplejavafx.mvc.model.entity.Entity;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 
 /**
  * Represents a piece disappearing from the board (e.g. via powerup).
  *
  * @param turn the turn number.
+ * @param entity the entity that disappeared.
  * @param position the position where the piece disappeared.
  */
-public record DespawnEvent(int turn, Point2D position) implements GameEvent {
+public record DespawnEvent(int turn, Entity entity, Point2D position) implements GameEvent {
     @Override
     public int getTurn() {
         return turn;
