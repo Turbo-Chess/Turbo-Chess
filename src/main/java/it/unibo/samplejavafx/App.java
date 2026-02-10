6eb461b4106d2fa50d3533328b3a3bfeb0a2b23c
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-10 10:57:54 +0100

    Added files to start up an empty gui

---
+++ src/main/java/it/unibo/samplejavafx/App.java
@@ -0,0 +1,21 @@
+package it.unibo.samplejavafx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 300);
+        stage.setTitle("JavaFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/it/unibo/samplejavafx/Main.java
@@ -0,0 +1,10 @@
+package it.unibo.samplejavafx;
+
+import it.unibo.samplejavafx.App;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
