250a65db5c811620e10819cb6f62b55670c1fa1d
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-10 14:38:14 +0100

    Updated app.java to look up for a scene and spot errors

--- src/main/java/it/unibo/samplejavafx/App.java
+++ src/main/java/it/unibo/samplejavafx/App.java
@@ -1,16 +1,31 @@
 package it.unibo.samplejavafx;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private static final String layoutsFolder = "src/main/resources/layouts/";
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 500, 300);
-        stage.setTitle("JavaFX");
+        var fxmlLocation = getClass().getResource( "/layouts/MainMenu.fxml");
+        if (fxmlLocation == null) {
+            throw new RuntimeException("CRITICAL: MainMenu.fxml not found in the classpath!");
+        }
+
+        Parent root = FXMLLoader.load(fxmlLocation);
+        Scene scene = new Scene(root);
+
+        var cssLocation = getClass().getResource("/css/MainMenu.css");
+        if (cssLocation != null) {
+            scene.getStylesheets().add(cssLocation.toExternalForm());
+        } else {
+            System.err.println("Warning: MainMenu.css not found, loading without styles.");
+        }
+
+        stage.setTitle("TurboChess - Main Menù");
         stage.setScene(scene);
         stage.show();
     }
