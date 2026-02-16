8511bc7dde2a269a70fafcd44e29d74dae971889
Brando Fabbri <brando.fabbri@studio.unibo.it>
2026-02-16 21:40:46 +0100

    Fixed TurnHandler interface implementation

--- src/main/java/it/unibo/samplejavafx/mvc/model/handler/TurnHandler.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/handler/TurnHandler.java
@@ -1,5 +1,10 @@
 package it.unibo.samplejavafx.mvc.model.handler;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
+
+import it.unibo.samplejavafx.mvc.model.movement.MoveRulesImpl.MoveType;
+import javafx.geometry.Point2D;
+
 /**
  * Placeholder.
  */
@@ -10,5 +15,5 @@ public interface TurnHandler {
      *
      * 
      */
-    void executeTurn();
+    boolean executeTurn(final MoveType moveAction, final Point2D target);
 }
