177fdcb2001b009a36ecfaafb6352b11da1a35b5
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-16 08:28:47 +0100

    Created a pseudoclass for signaling piece movement displayed or not

---
+++ src/main/java/it/unibo/samplejavafx/mvc/view/ChessboardViewPseudoClasses.java
@@ -0,0 +1,8 @@
+package it.unibo.samplejavafx.mvc.view;
+
+import javafx.css.PseudoClass;
+
+public class ChessboardViewPseudoClasses {
+    public static final PseudoClass VALID_MOVEMENT_CELL = PseudoClass.getPseudoClass("valid_movement");
+
+}
