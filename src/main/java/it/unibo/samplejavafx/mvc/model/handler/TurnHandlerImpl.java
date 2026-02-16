d9b144b5951e09c52ada43a20640a44f2988c2be
Brando Fabbri <brando.fabbri@studio.unibo.it>
2026-02-16 22:09:12 +0100

    Complete implementation of TurnHandler

--- src/main/java/it/unibo/samplejavafx/mvc/model/handler/TurnHandlerImpl.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/handler/TurnHandlerImpl.java
@@ -2,31 +2,38 @@ package it.unibo.samplejavafx.mvc.model.handler;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import it.unibo.samplejavafx.mvc.model.chessboard.ChessBoard;
 import it.unibo.samplejavafx.mvc.model.entity.Piece;
+import it.unibo.samplejavafx.mvc.model.entity.PieceType;
 import it.unibo.samplejavafx.mvc.model.entity.PlayerColor;
-import it.unibo.samplejavafx.mvc.model.movement.MoveRulesImpl.MoveStrategy;
 import it.unibo.samplejavafx.mvc.model.movement.MoveRulesImpl.MoveType;
 import it.unibo.samplejavafx.mvc.model.point2d.Point2D;
 import it.unibo.samplejavafx.mvc.model.rules.AdvancedRules;
+import it.unibo.samplejavafx.mvc.model.rules.CastleCondition;
+import it.unibo.samplejavafx.mvc.model.rules.CheckCalculator;
 
 /**
  * Placeholder.
  */
-public class TurnHandlerImpl {
+public class TurnHandlerImpl implements TurnHandler {
     private int turn;
     private ChessBoard board;
     private GameState state;
+    private CastleCondition castlingOptions;
     private PlayerColor currentColor;
     private Optional<Piece> currentPiece;
     private List<Point2D> pieceMoves;
+    private Map<Piece, List<Point2D>> interposingPieces;
 
     public TurnHandlerImpl(final int turn, final ChessBoard board) {
         this.turn = turn;
         this.board = board;
         this.currentColor = PlayerColor.WHITE;
+        this.castlingOptions = CastleCondition.NO_CASTLE;
+        this.state = GameState.NORMAL;
     }
 
     /**
@@ -36,12 +43,69 @@ public class TurnHandlerImpl {
      * @return placeholder.
      */
     public List<Point2D> thinking(final Point2D pos) {
+        if (state.equals(GameState.NORMAL)) {
+            return doIfNormal(pos);
+        }
+        if (state.equals(GameState.CHECK)) {
+            return doIfCheck(pos);
+        }
+        if (state.equals(GameState.DOUBLE_CHECK)) {
+            return doIfDoubleCheck(pos);
+        }
+        return Collections.emptyList(); // unreachable for design
+    }
+
+    /**
+     *  placeholder.
+     *
+     * @param moveAction placeholder.
+     * @param target placeholder.
+     */
+    @Override
+    public boolean executeTurn(final MoveType moveAction, final Point2D target) {
+        if (!CheckCalculator.isMoveSafe(board, currentPiece.get(), 
+            board.getPosByEntity(currentPiece.get()), target, currentColor)) {
+                return false; // the move wasn't safe, so we cancel it and go back
+        }
+        switch (moveAction) {
+            case MOVE_ONLY: 
+                board.move(board.getPosByEntity(currentPiece.get()), target);
+                break;
+            case MOVE_AND_EAT:
+                board.eat(board.getPosByEntity(currentPiece.get()), target);
+                break;
+            default:
+                // the move wasn't safe, so we cancel the move and go back
+        }
+        this.state = AdvancedRules.check(board, AdvancedRules.swapColor(currentColor));
+        if (state.equals(GameState.CHECK) || state.equals(GameState.DOUBLE_CHECK)) {
+            if (AdvancedRules.checkmate(board, AdvancedRules.swapColor(currentColor), state, interposingPieces)) {
+                // call to another function that ends the match
+            }
+        }
+        if (AdvancedRules.draw(board, AdvancedRules.swapColor(currentColor))) {
+            // call to another function that ends the match
+        }
+
+        // Changing variables for the next turn iteration
+        this.castlingOptions = AdvancedRules.castle(board, AdvancedRules.swapColor(currentColor));
+        this.turn += 1;
+        this.currentColor = AdvancedRules.swapColor(currentColor);
+        return true;
+    }
+
+    /**
+     * placeholder.
+     * 
+     * @param pos placeholder.
+     * @return placeholder.
+     */
+    private List<Point2D> doIfNormal(final Point2D pos) {
         if (board.isFree(pos) && currentPiece.isEmpty()) {
             return Collections.emptyList();
         }
         if (board.isFree(pos) && pieceMoves.contains(pos)) {
-            executeTurn(MoveType.MOVE_ONLY, pos);
-            return List.of(pos);
+            return executeTurn(MoveType.MOVE_ONLY, pos) ? List.of(pos) : Collections.emptyList();
         }
         if (!board.isFree(pos) && board.getEntity(pos).get().getPlayerColor().equals(currentColor)) {
             var newPiece = (Piece) board.getEntity(pos).get();
@@ -51,34 +115,92 @@ public class TurnHandlerImpl {
         }
         if (!board.isFree(pos)
             && board.getEntity(pos).get().getPlayerColor().equals(AdvancedRules.swapColor(currentColor))
-            && currentPiece.isPresent()) {
-            if (pieceMoves.contains(pos)) {
-                executeTurn(MoveType.MOVE_AND_EAT, pos);
-                return List.of(pos);
-            }
+            && currentPiece.isPresent() && pieceMoves.contains(pos)) {
+            return executeTurn(MoveType.MOVE_AND_EAT, pos) ? List.of(pos) : Collections.emptyList();
         }
-        this.currentPiece = Optional.empty();
-        this.pieceMoves = Collections.emptyList();
+        unsetCurrentPiece();
         return this.pieceMoves;
     }
 
     /**
-     *  placeholder.
-     *
-     * @param moveAction placeholder.
-     * @param target placeholder.
+     * placeholder.
+     * 
+     * @param pos placeholder.
+     * @return placeholder.
      */
-    public void executeTurn(final MoveType moveAction, final Point2D target) {
-        switch (moveAction) {
-            case MOVE_ONLY:
-
-                break;
-            case MOVE_AND_EAT:
-
-                break;
-            default:
-
-                break;
+    private List<Point2D> doIfCheck(final Point2D pos) {
+        if (board.isFree(pos) && currentPiece.isEmpty()) {
+            return Collections.emptyList();
         }
+        if (board.isFree(pos) && pieceMoves.contains(pos)) {
+            return executeTurn(MoveType.MOVE_ONLY, pos) ? List.of(pos) : Collections.emptyList();
+        }
+        if (!board.isFree(pos) && board.getEntity(pos).get().getPlayerColor().equals(currentColor)
+            && board.getEntity(pos).get().getType().equals(PieceType.KING)) {
+            var king = (Piece) board.getEntity(pos).get();
+            this.currentPiece = Optional.of(king);
+            this.pieceMoves = AdvancedRules.kingPossibleMoves(king.getValidMoves(pos, board), board, currentColor);
+            return this.pieceMoves;
+        }
+        if (!board.isFree(pos) && board.getEntity(pos).get().getPlayerColor().equals(currentColor)
+            && interposingPieces.keySet().contains(board.getEntity(pos).get())) {
+            var piece = (Piece) board.getEntity(pos).get();
+            this.currentPiece = Optional.of(piece);
+            this.pieceMoves = interposingPieces.get(piece);
+            return this.pieceMoves;
+        }
+        if (!board.isFree(pos)
+            && board.getEntity(pos).get().getPlayerColor().equals(AdvancedRules.swapColor(currentColor))
+            && currentPiece.isPresent() && pieceMoves.contains(pos)) {
+            return executeTurn(MoveType.MOVE_AND_EAT, pos) ? List.of(pos) : Collections.emptyList();
+        }
+        unsetCurrentPiece();
+        return this.pieceMoves;
+    }
+
+    /**
+     * placeholder.
+     * 
+     * @param pos placeholder.
+     * @return placeholder.
+     */
+    private List<Point2D> doIfDoubleCheck(final Point2D pos) {
+        if (board.isFree(pos) && currentPiece.isEmpty()) {
+            return Collections.emptyList();
+        }
+        if (board.isFree(pos) && pieceMoves.contains(pos)) {
+            return executeTurn(MoveType.MOVE_ONLY, pos) ? List.of(pos) : Collections.emptyList();
+        }
+        if (!board.isFree(pos) && board.getEntity(pos).get().getPlayerColor().equals(currentColor)
+            && board.getEntity(pos).get().getType().equals(PieceType.KING)) {
+            var king = (Piece) board.getEntity(pos).get();
+            this.currentPiece = Optional.of(king);
+            this.pieceMoves = AdvancedRules.kingPossibleMoves(king.getValidMoves(pos, board), board, currentColor);
+            return this.pieceMoves;
+        }
+        if (!board.isFree(pos)
+            && board.getEntity(pos).get().getPlayerColor().equals(AdvancedRules.swapColor(currentColor))
+            && currentPiece.isPresent() && pieceMoves.contains(pos)) {
+            return executeTurn(MoveType.MOVE_AND_EAT, pos) ? List.of(pos) : Collections.emptyList();
+        }
+        unsetCurrentPiece();
+        return this.pieceMoves;
+    }
+
+    /**
+     * placeholder.
+     */
+    private void unsetCurrentPiece() {
+        this.currentPiece = Optional.empty();
+        this.pieceMoves = Collections.emptyList();
+    }
+
+    /**
+     * placeholder.
+     * 
+     * @return placeholder.
+     */
+    public int getCurrentTurn() {
+        return this.turn;
     }
 }
