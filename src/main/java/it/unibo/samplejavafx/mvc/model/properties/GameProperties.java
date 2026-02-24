b6b98a711c8738227b4e181198276ceda9b8504c
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-02-24 14:15:08 +0100

    Fixed naming error

--- src/main/java/it/unibo/samplejavafx/mvc/model/properties/GameProperties.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/properties/GameProperties.java
@@ -2,12 +2,14 @@ package it.unibo.samplejavafx.mvc.model.properties;
 
 import lombok.Getter;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public enum GameProperties {
     USER_HOME_FOLDER(System.getProperty("user.home")),
     ROOT_RESOURCE_FOLDER("file:" + Paths.get(USER_HOME_FOLDER.getPath(), "/.TurboChess").toString()),
-    MOD_FOLDER(Paths.get(ROOT_RESOURCE_FOLDER.getPath(), "/Mods").toString()),
+    EXTERNAL_MOD_FOLDER(Paths.get(ROOT_RESOURCE_FOLDER.getPath(), "/Mods").toString()),
     EXTERNAL_ASSETS_FOLDER(Paths.get(ROOT_RESOURCE_FOLDER.getPath() + "/Assets").toString()),
     INTERNAL_ASSETS_FOLDER("classpath:/assets"),
     INTERNAL_ENTITIES_FOLDER("classpath:/EntityResources");
