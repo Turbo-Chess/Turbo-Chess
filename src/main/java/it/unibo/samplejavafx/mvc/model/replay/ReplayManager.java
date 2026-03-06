2ca32c4ac4ddab57b1cef888f4601f976182ea29
Luca Bevilacqua <luca.bevilacqua6@studio.unibo.it>
2026-03-06 17:47:30 +0100

    fix: register Jackson modules for proper JSON serialization

--- src/main/java/it/unibo/samplejavafx/mvc/model/replay/ReplayManager.java
+++ src/main/java/it/unibo/samplejavafx/mvc/model/replay/ReplayManager.java
@@ -24,6 +24,7 @@ public class ReplayManager {
      */
     public ReplayManager() {
         this.mapper = new ObjectMapper();
+        this.mapper.findAndRegisterModules();
         this.mapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
